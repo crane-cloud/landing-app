4a509465354a8ce811a64170cc02689567299496
Mubangizi Allan <mubangizia22@gmail.com>
2025-05-17 13:19:30 +0300

    add hero section

---
+++ src/assets/images/images.ts
@@ -0,0 +1,8 @@
+// import logo from "./icons/logo.svg";
+// import logoWhite from "./icons/logo-white.svg";
+import logoPng from "./logo.png";
+import logoSvg from "./logo.svg";
+
+// export const LogoSvg = logo;
+export const LogoWhiteSvg = logoPng;
+export const FaviconSvg = logoSvg;
