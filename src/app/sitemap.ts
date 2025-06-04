d3a31e9ae1dc70ea231a41d78cdb615efa2c468c
Mubangizi Allan <mubangizia22@gmail.com>
2025-06-04 12:19:38 +0300

    fix variable issue

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,35 +1,35 @@
-import { currentUrl } from '@/assets/data';
+import { CURRENT_URL } from '@/assets/data';
 import { MetadataRoute } from 'next';
 
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: currentUrl,
+      url: CURRENT_URL,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: `${currentUrl}/about`,
+      url: `${CURRENT_URL}/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${currentUrl}/contact`,
+      url: `${CURRENT_URL}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${currentUrl}/privacy-policy`,
+      url: `${CURRENT_URL}/privacy-policy`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     },
     {
-      url: `${currentUrl}/terms-of-service`,
+      url: `${CURRENT_URL}/terms-of-service`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
