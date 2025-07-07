d85a78a39f3945856391c3c726554b3aa98381ec
Khalifan Muwonge <khalifanmuwonge@gmail.com>
2025-07-07 13:49:30 +0300

    Revert " changed the stlying of the menu open button from inlineflex to block …"

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -122,7 +122,8 @@ export default function Header() {
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className={`"-m-2.5 block items-center justify-center rounded-md p-2.5 text-gray-700" ${mobileMenuOpen ? "hidden":"block"}`}>
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
