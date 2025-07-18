aa0502c76509f1770e97d239e0dbe5975bbda36f
Mubangizi Allan <mubangizia22@gmail.com>
2025-07-18 12:46:40 +0300

    add 404 page

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -4,9 +4,11 @@ import logoPng from "./logo.png";
 import logoSvg from "./logo.svg";
 import logoWhiteSvg from "./logo-white.svg";
 import logoDarkSvg from "./logo-dark.svg";
+import notFoundSvg from "./not-found.svg";
 
 // export const LogoSvg = logo;
 export const LogoSvg = logoPng;
 export const LogoWhiteSvg = logoWhiteSvg;
 export const FaviconSvg = logoSvg;
 export const LogoDarkSvg = logoDarkSvg;
+export { notFoundSvg };
